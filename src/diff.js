48accecc3bba6ebd0326f697d57076359f1dd72a
Marius Gundersen <me@mariusgundersen.net>
2016-10-29 12:53:34 +0200

    Improved diff using unit tests

--- src/diff.js
+++ src/diff.js
@@ -1,7 +1,7 @@
 import intersect from './intersect.js';
 import createNode from './createNode.js';
 
-export default function *diff(treeBefore, treeAfter, top=0, left=0){
+export default function *diff(treeBefore=null, treeAfter=null, top=0, left=0){
   if(treeBefore === treeAfter){
     return;
   }
@@ -14,28 +14,42 @@ export default function *diff(treeBefore, treeAfter, top=0, left=0){
     treeAfter = createNode(treeBefore.size);
   }
 
-  if(treeBefore.data !== null && treeBefore.data !== treeAfter.data){
+  if(treeBefore.data !== null && treeAfter.data === null){
     yield clear(treeBefore.data, {}, top, left);
   }
 
-  if(treeBefore.center !== null && treeBefore.center !== treeAfter.center){
+  if(treeBefore.center !== null
+  && (treeAfter.center === null
+  || !sameBox(treeBefore.center, treeAfter.center))){
     yield clear(treeBefore.center.data, treeBefore.center, top, left);
   }
 
   if(treeBefore.top !== treeAfter.top){
-    yield* treeBefore.top.filter(t => treeAfter.top.indexOf(t) < 0).map(t => clear(t.data, t, top, left));
+    yield* treeBefore.top
+      .filter(b => treeAfter.top.indexOf(b) < 0)
+      .filter(b => !treeAfter.top.some(a => sameBox(a, b)))
+      .map(t => clear(t.data, t, top, left));
   }
 
   if(treeBefore.left !== treeAfter.left){
-    yield* treeBefore.left.filter(t => treeAfter.left.indexOf(t) < 0).map(t => clear(t.data, t, top, left));
+    yield* treeBefore.left
+      .filter(b => treeAfter.left.indexOf(b) < 0)
+      .filter(b => !treeAfter.left.some(a => sameBox(a, b)))
+      .map(t => clear(t.data, t, top, left));
   }
 
   if(treeBefore.right !== treeAfter.right){
-    yield* treeBefore.right.filter(t => treeAfter.right.indexOf(t) < 0).map(t => clear(t.data, t, top, left));
+    yield* treeBefore.right
+      .filter(b => treeAfter.right.indexOf(b) < 0)
+      .filter(b => !treeAfter.right.some(a => sameBox(a, b)))
+      .map(t => clear(t.data, t, top, left));
   }
 
   if(treeBefore.bottom !== treeAfter.bottom){
-    yield* treeBefore.bottom.filter(t => treeAfter.bottom.indexOf(t) < 0).map(t => clear(t.data, t, top, left));
+    yield* treeBefore.bottom
+      .filter(b => treeAfter.bottom.indexOf(b) < 0)
+      .filter(b => !treeAfter.bottom.some(a => sameBox(a, b)))
+      .map(t => clear(t.data, t, top, left));
   }
 
   const halfSize = treeBefore.size/2;
@@ -44,28 +58,40 @@ export default function *diff(treeBefore, treeAfter, top=0, left=0){
   yield* diff(treeBefore.bottomLeft, treeAfter.bottomLeft, top+halfSize, left);
   yield* diff(treeBefore.bottomRight, treeAfter.bottomRight, top+halfSize, left+halfSize);
 
-  if(treeBefore.center !== treeAfter.center && treeAfter.center !== null){
-    yield set(treeBefore.center.data, treeBefore.center, top, left);
+  if(treeAfter.center !== null){
+    if(treeBefore.center === null || !sameBox(treeBefore.center, treeAfter.center)){
+      yield set(treeAfter.center.data, treeAfter.center, top, left);
+    }else if(treeBefore.center !== treeAfter.center && sameBox(treeBefore.center, treeAfter.center)){
+      yield update(treeBefore.center.data, treeAfter.center.data, treeAfter.center, top, left);
+    }
   }
 
   if(treeBefore.top !== treeAfter.top){
-    yield* treeAfter.top.filter(t => treeBefore.top.indexOf(t) < 0).map(t => set(t.data, t, top, left));
+    yield* getSet(treeBefore.top, treeAfter.top, top, left);
+    yield* getUpdate(treeBefore.top, treeAfter.top, top, left);
   }
 
   if(treeBefore.left !== treeAfter.left){
-    yield* treeAfter.left.filter(t => treeBefore.left.indexOf(t) < 0).map(t => set(t.data, t, top, left));
+    yield* getSet(treeBefore.left, treeAfter.left, top, left);
+    yield* getUpdate(treeBefore.left, treeAfter.left, top, left);
   }
 
   if(treeBefore.right !== treeAfter.right){
-    yield* treeAfter.right.filter(t => treeBefore.right.indexOf(t) < 0).map(t => set(t.data, t, top, left));
+    yield* getSet(treeBefore.right, treeAfter.right, top, left);
+    yield* getUpdate(treeBefore.right, treeAfter.right, top, left);
   }
 
   if(treeBefore.bottom !== treeAfter.bottom){
-    yield* treeAfter.bottom.filter(t => treeBefore.bottom.indexOf(t) < 0).map(t => set(t.data, t, top, left));
+    yield* getSet(treeBefore.bottom, treeAfter.bottom, top, left);
+    yield* getUpdate(treeBefore.bottom, treeAfter.bottom, top, left);
   }
 
-  if(treeBefore.data !== treeAfter.data && treeAfter.data !== null){
-    yield set(treeAfter.data, {}, top, left);
+  if(treeAfter.data !== null){
+    if(treeBefore.data === null){
+      yield set(treeAfter.data, {}, top, left);
+    }else if(treeBefore.data !== treeAfter.data){
+      yield update(treeBefore.data, treeAfter.data, {}, top, left);
+    }
   }
 }
 
@@ -89,4 +115,42 @@ function set(after, {top=0, left=0, right=left+1, bottom=top+1, width=right-left
     height,
     after
   };
+}
+
+function update(before, after, {top=0, left=0, right=left+1, bottom=top+1, width=right-left, height=bottom-top}, treeTop, treeLeft){
+  return {
+    type: 'update',
+    top: treeTop + top,
+    left: treeLeft + left,
+    width,
+    height,
+    before,
+    after
+  };
+}
+
+function getSet(befores, afters, top, left){
+  return afters
+    .filter(after => befores.indexOf(after) < 0)
+    .filter(after => !befores.some(before => sameBox(before, after)))
+    .map(t => set(t.data, t, top, left));
+}
+
+function getUpdate(befores, afters, top, left){
+  return afters
+    .filter(after => befores.indexOf(after) < 0)
+    .map(after => ({
+      before: befores.filter(before => sameBox(before, after))[0],
+      after
+    }))
+    .filter(t => t.before)
+    .map(t => update(t.before.data, t.after.data, t.after, top, left))
+}
+
+function sameBox(a, b){
+  if(a.top !== b.top) return false;
+  if(a.left !== b.left) return false;
+  if(a.right !== b.right) return false;
+  if(a.bottom !== b.bottom) return false;
+  return true;
 }
---
+++ test/diff.js
@@ -0,0 +1,278 @@
+import test from 'ava';
+
+import diff from '../src/diff.js';
+import set from '../src/set.js';
+import createNode from '../src/createNode.js';
+
+test('null and null', t => {
+  const result = [...diff(null, null)];
+  t.is(result.length, 0);
+});
+
+test('same', t => {
+  const tree = createNode(1);
+  const result = [...diff(tree, tree)];
+  t.is(result.length, 0);
+});
+
+test('set empty tree', t => {
+  const tree = createNode(1);
+  const result = [...diff(null, tree)];
+  t.is(result.length, 0);
+});
+
+test('clear empty tree', t => {
+  const tree = createNode(1);
+  const result = [...diff(tree, null)];
+  t.is(result.length, 0);
+});
+
+test('two empty trees', t => {
+  const treeBefore = createNode(1);
+  const treeAfter = createNode(1);
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 0);
+});
+
+test('set data', t => {
+  const treeBefore = {
+    ...createNode(1)
+  };
+  const treeAfter = {
+    ...createNode(1),
+    data: 'hello'
+  };
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'set',
+    top: 0,
+    left: 0,
+    width: 1,
+    height: 1,
+    after: 'hello'
+  });
+});
+
+test('clear data', t => {
+  const treeBefore = {
+    ...createNode(1),
+    data: 'hello'
+  };
+  const treeAfter = {
+    ...createNode(1)
+  };
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'clear',
+    top: 0,
+    left: 0,
+    width: 1,
+    height: 1,
+    before: 'hello'
+  });
+});
+
+test('update data', t => {
+  const treeBefore = {
+    ...createNode(1),
+    data: 'hello'
+  };
+  const treeAfter = {
+    ...createNode(1),
+    data: 'hi'
+  };
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'update',
+    top: 0,
+    left: 0,
+    width: 1,
+    height: 1,
+    before: 'hello',
+    after: 'hi'
+  });
+});
+
+test('set and clear deep data', t => {
+  const treeBefore = createTree(4, {data: 'hello', top:1, left: 2});
+  const treeAfter = createTree(4, {data: 'hello', top:2, left: 1});
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 2);
+  t.deepEqual(result[0], {
+    type: 'clear',
+    top: 1,
+    left: 2,
+    width: 1,
+    height: 1,
+    before: 'hello'
+  });
+  t.deepEqual(result[1], {
+    type: 'set',
+    top: 2,
+    left: 1,
+    width: 1,
+    height: 1,
+    after: 'hello'
+  });
+});
+
+test('same big block center', t => {
+  const tree = createTree(4, {data: 'hello', top:1, left: 1, width: 2, height: 2});
+  const result = [...diff(tree, tree)];
+  t.is(result.length, 0);
+});
+
+test('set big block center', t => {
+  const treeBefore = createTree(4);
+  const treeAfter = createTree(4, {data: 'hello', top:1, left: 1, width: 2, height: 2});
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'set',
+    top: 1,
+    left: 1,
+    width: 2,
+    height: 2,
+    after: 'hello'
+  });
+});
+
+test('clear big block center', t => {
+  const treeBefore = createTree(4, {data: 'hello', top:1, left: 1, width: 2, height: 2});
+  const treeAfter = createTree(4);
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'clear',
+    top: 1,
+    left: 1,
+    width: 2,
+    height: 2,
+    before: 'hello'
+  });
+});
+
+test('update big block center', t => {
+  const treeBefore = createTree(4, {data: 'hello', top:1, left: 1, width: 2, height: 2});
+  const treeAfter = createTree(4, {data: 'hi', top:1, left: 1, width: 2, height: 2});
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'update',
+    top: 1,
+    left: 1,
+    width: 2,
+    height: 2,
+    before: 'hello',
+    after: 'hi'
+  });
+});
+
+test('change big block size center', t => {
+  const treeBefore = createTree(4, {data: 'hello', top:1, left: 1, width: 2, height: 2});
+  const treeAfter = createTree(4, {data: 'hello', top:0, left: 0, width: 4, height: 4});
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 2);
+  t.deepEqual(result[0], {
+    type: 'clear',
+    top: 1,
+    left: 1,
+    width: 2,
+    height: 2,
+    before: 'hello'
+  });
+  t.deepEqual(result[1], {
+    type: 'set',
+    top: 0,
+    left: 0,
+    width: 4,
+    height: 4,
+    after: 'hello'
+  });
+});
+
+test('same big block top', t => {
+  const tree = createTree(4, {data: 'hello', top:0, left: 1, width: 2, height: 1});
+  const result = [...diff(tree, tree)];
+  t.is(result.length, 0);
+});
+
+test('set big block top', t => {
+  const treeBefore = createTree(4);
+  const treeAfter = createTree(4, {data: 'hello', top:0, left: 1, width: 2, height: 1});
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'set',
+    top: 0,
+    left: 1,
+    width: 2,
+    height: 1,
+    after: 'hello'
+  });
+});
+
+test('clear big block top', t => {
+  const treeBefore = createTree(4, {data: 'hello', top:0, left: 1, width: 2, height: 1});
+  const treeAfter = createTree(4);
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'clear',
+    top: 0,
+    left: 1,
+    width: 2,
+    height: 1,
+    before: 'hello'
+  });
+});
+
+test('update big block top', t => {
+  const treeBefore = createTree(4, {data: 'hello', top:0, left: 1, width: 2, height: 1});
+  const treeAfter = createTree(4, {data: 'hi', top:0, left: 1, width: 2, height: 1});
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 1);
+  t.deepEqual(result[0], {
+    type: 'update',
+    top: 0,
+    left: 1,
+    width: 2,
+    height: 1,
+    before: 'hello',
+    after: 'hi'
+  });
+});
+
+test('resize big block top', t => {
+  const treeBefore = createTree(4, {data: 'hello', top:0, left: 1, width: 2, height: 1});
+  const treeAfter = createTree(4, {data: 'hello', top:0, left: 1, width: 2, height: 2});
+  const result = [...diff(treeBefore, treeAfter)];
+  t.is(result.length, 2);
+  t.deepEqual(result[0], {
+    type: 'clear',
+    top: 0,
+    left: 1,
+    width: 2,
+    height: 1,
+    before: 'hello'
+  });
+  t.deepEqual(result[1], {
+    type: 'set',
+    top: 0,
+    left: 1,
+    width: 2,
+    height: 2,
+    after: 'hello'
+  });
+});
+
+function createTree(size, ...data){
+  let tree = createNode(size);
+  for(let entry of data){
+    tree = set(tree, entry.data, entry);
+  }
+  return tree;
+}
